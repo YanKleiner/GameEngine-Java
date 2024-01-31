3a7ac0aa0b879fcedd691c4bb0e1eaf4feb80ea0
YanKleiner <yankleiner22@gmail.com>
2024-01-31 18:18:00 +0100

    dd

--- src/com/yankleiner/engine/Renderer.java
+++ src/com/yankleiner/engine/Renderer.java
@@ -1,7 +1,7 @@
 package com.yankleiner.engine;
 import java.awt.image.DataBufferInt;
 public class Renderer {
-	public static int test = 0;
+	
 	private int pW, pH;
 	private int[] pixels;
 	public Renderer(GameContainer gc) {
@@ -13,13 +13,7 @@ public class Renderer {
 	}
 	public void clear() {
 		for(int i = 0 ; i < pixels.length; i++) {
-			pixels[i] += -i;
-			while(test == 1) {
-				pixels[i] -= 200000000;
-				System.out.println("- 200");
-				test = 0 ;
-			}
-			//pixels[i] += i/2000;
+			pixels[i] += i;
 			
 		}
 	}
